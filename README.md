0f48e877f3b40ee551b6eb37363ee3eb0f454fca
teevik <teemu.vikoren@gmail.com>
2025-10-29 13:55:45 +0100

    Update nix, add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# GPU Raytracer
+
+A compute shader raytracer implementation using [rust-gpu](https://github.com/EmbarkStudios/rust-gpu)
+
+## Showcase
+
+![Raytracing Result](image.png)
--- flake.nix
+++ flake.nix
@@ -4,27 +4,29 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-    perSystem = { pkgs, ... }: {
-      devShells.default = pkgs.mkShell {
-        hardeningDisable = [ "fortify" ];
-
-        packages = with pkgs; [
-          pkg-config
-
-          udev
-          alsa-lib
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          libxkbcommon
-          wayland
-        ];
-      };
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              vulkan-loader
+              stdenv.cc.cc
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+            ];
+          };
+        };
     };
-  };
 }
